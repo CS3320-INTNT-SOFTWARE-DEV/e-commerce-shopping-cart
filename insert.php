5a5997c69ea9ea1357688ae9d51cae7dbd3ae13c
migGarciagim <54682636+migGarciagim@users.noreply.github.com>
2019-11-10 20:31:53 -0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,25 +1,31 @@
 <?php
 
-    
     $user = 'root';
     $pass = '';
     $db = 'cs3320';
 
     $conn = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
 
-    $fullname = $_POST['userfullname'];
-    $address1 = $_POST['useraddress1'];
-    $address2 = $_POST['useraddress2'];
-    $city = $_POST['usercity'];
-    $state = $_POST['userstate'];
-    $zip = $_POST['userzipcode'];
-    $phone = $_POST['userphone'];
-    $email = $_POST['useremail'];
-
+    // User Information
+    if(isset($_POST['userfullname'])){
+        
     $sql = "INSERT INTO userinformation(userId, fullname, address1, address2, city, state, zip, phone, email) 
-            VALUES('1', '$fullname', '$address1', '$address2', '$city', '$state', '$zip', '$phone', '$email')";
+            VALUES  ('1', ' $_POST[userfullname]', '$_POST[useraddress1]', '$_POST[useraddress2]', 
+                    '$_POST[usercity]', '$_POST[userstate]', '$_POST[userzipcode]', '$_POST[userphone]', 
+                    '$_POST[useremail]')";
+    }//end if
 
-   //execute the INSERT
+    // Shipping Information
+    if(isset($_POST['shippingaddress1'])){
+
+    $sql = "INSERT INTO shippinginformation(userId, address1, address2, city, state, zip) 
+            VALUES  ('1', '$_POST[shippingaddress1]', '$_POST[shippingaddress2]', '$_POST[shippingcity]', 
+                    '$_POST[shippingstate]', '$_POST[shippingzipcode]')";
+    }//end if
+
+
+
+   //execute the INSERT 
    if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 
